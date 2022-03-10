b46f391323f9036133daa3690ae08ec18174782b
BlackPanther <admin@BlackPanthers-MacBook-Pro.local>
2022-03-10 11:09:36 +0900

    CMD

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -25,8 +25,8 @@ export default function ProductList({ kind }: ProductListProps) {
                     key={record.id}
                     title={record.product.name}
                     price={record.product.price}
-                    comments={1}
                     hearts={record.product._count.fav}
+                    image={record.product.image}
                 />
             ))}
         </>
